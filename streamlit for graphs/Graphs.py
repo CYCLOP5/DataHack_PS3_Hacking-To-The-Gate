c226a2963e5a7335c27e41bbd40b85ee78089abc
Varun J <vnjhaveri@gmail.com>
2023-10-29 10:53:39 +0530

    final v final v final

--- streamlit for graphs/Graphs.py
+++ streamlit for graphs/Graphs.py
@@ -13,6 +13,34 @@ df["bcEnd_Time"] = pd.to_datetime(df["bcEnd_Time"])
 df["sStart_Time"] = pd.to_datetime(df["sStart_Time"])
 df["sEnd_Time"] = pd.to_datetime(df["sEnd_Time"])
 
+sets_df = pd.DataFrame(
+    {
+        "Exercise": ["Squats", "Pushups", "BicepCurls"],
+        "Sets": [
+            df["Squats_Sets"].sum(),
+            df["Pushups_Sets"].sum(),
+            df["BicepCurls_Sets"].sum(),
+        ],
+    }
+)
+
+pie_chart = (
+    alt.Chart(sets_df)
+    .mark_arc()
+    .encode(
+        theta="Sets",
+        color=alt.Color(
+            "Exercise", scale=alt.Scale(range=["#1f77b4", "#ff7f0e", "#2ca02c"])
+        ),
+        tooltip=["Exercise", "Sets"],
+        y=alt.value(1),
+    )
+    .properties(width=500, height=500)
+)
+
+st.write(pie_chart)
+
+
 bar_chart1 = (
     alt.Chart(df)
     .mark_bar()
@@ -49,33 +77,6 @@ bar_chart3 = (
 
 st.write(bar_chart3)
 
-sets_df = pd.DataFrame(
-    {
-        "Exercise": ["Squats", "Pushups", "BicepCurls"],
-        "Sets": [
-            df["Squats_Sets"].sum(),
-            df["Pushups_Sets"].sum(),
-            df["BicepCurls_Sets"].sum(),
-        ],
-    }
-)
-
-pie_chart = (
-    alt.Chart(sets_df)
-    .mark_arc()
-    .encode(
-        theta="Sets",
-        color=alt.Color(
-            "Exercise", scale=alt.Scale(range=["#1f77b4", "#ff7f0e", "#2ca02c"])
-        ),
-        tooltip=["Exercise", "Sets"],
-        y=alt.value(1),
-    )
-    .properties(width=500, height=500)
-)
-
-st.write(pie_chart)
-
 
 time_series_chart = (
     alt.Chart(df)
@@ -89,7 +90,13 @@ time_series_chart = (
 )
 
 st.write(time_series_chart)
+st.write(
+    "\n## The threshold values are the multiplication of the number of sets x number of individual reps divided by time taken for each set. "
+)
 
-# df2 = pd.read_csv("./meow.csv")
-
-# st.write(df2)
+st.write(
+    "### This value give us a constant number for each set and after analying the data for all the sets we can aquare a median and around the median we can decided if the user is nearing exhaustion or has more energy left after the set."
+)
+df2 = pd.read_csv("./meow.csv")
+df2 = df2.drop(["Unnamed: 0", "Unnamed: 5"], axis=1)
+st.write(df2)
